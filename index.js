0851de6878bd69d89dcf9e9881def4d378c6d2cf
Jimkwan <jimkwan.yau@gmail.com>
2019-04-12 12:03:02 +0800

    fix

--- index.js
+++ index.js
@@ -9,27 +9,31 @@ const package = require('./package')
 const { exec } = require('child_process')
 
 program.version(package.version)
-  .option('-i, --init [app]', 'init a kurge scaffold', 'kurge')
+  .option('-i, --init [app]', 'init a kurge scaffold')
   .parse(process.argv)
 
-const spinner = ora('Generating app...').start()
+if (typeof program.init !== 'string' || !program.init) {
+  program.help()
+}
+
 if (fs.existsSync(program.init)) {
   throw new Error(`${program.init} app has already exists in current directory`)
-} else {
-  ncp(path.resolve(__dirname, 'templates/parcel'), program.init, function (err) {
-    if (err) {
-      throw err
-    } else {
-      spinner.succeed('Generated app')
-      spinner.start('Installing dependecies...')
-      exec(`cd ${program.init}; npm install; cd ..;`, function (err) {
-        if (err) {
-          throw err
-        } else {
-          spinner.succeed('Installed dependecies')
-          spinner.info(`Success! Happy coding!\n\n  cd ${program.init}\n  npm start\n`)
-        }
-      })
-    }
-  })
 }
+
+const spinner = ora(`Generating ${program.init} app...`).start()
+ncp(path.resolve(__dirname, 'templates/parcel'), program.init, function (err) {
+  if (err) {
+    throw err
+  } else {
+    spinner.succeed(`Generated ${program.init} app`)
+    spinner.start('Installing dependecies...')
+    exec(`cd ${program.init}; npm install; cd ..;`, function (err) {
+      if (err) {
+        throw err
+      } else {
+        spinner.succeed('Installed dependecies')
+        spinner.info(`Success! Happy coding!\n\n  cd ${program.init}\n  npm start\n`)
+      }
+    })
+  }
+})
